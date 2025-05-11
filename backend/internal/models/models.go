ad9ac51dcbc4fd3232600ca755874712ea5dee54
ZhuoyangM <zhmeng@ucsd.edu>
2025-05-10 23:03:06 -0400

    docker & models

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -2,14 +2,31 @@ package models
 
 import (
 	"time"
-
-	"github.com/google/uuid"
 )
 
+// pointer indicates that the field is nullable
+type User struct {
+	ID       uint   `gorm:"primaryKey"`
+	Username string `gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+	Email    string `gorm:"unique;not null"`
+}
+
 type ScanJob struct {
-	ID        uuid.UUID  `json:"id"`
-	UserID    uuid.UUID  `json:"user_id"`
-	TargetUrl string     `json:"target_url"`
-	CreatedAt *time.Time `json:"created_at"`
-	Status    string     `json:"status"`
+	ID        uint      `gorm:"primaryKey"`
+	UserID    uint      `gorm:"not null"`
+	User      User      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	TargetUrl string    `gorm:"not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	Status    string    `gorm:"not null"` // "running", "completed", "failed"
+}
+
+type ScanResult struct {
+	ID            uint    `gorm:"primaryKey"`
+	ScanJobID     uint    `gorm:"not null"`
+	ScanJob       ScanJob `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ScanUrl       string  `gorm:"not null"`
+	Status        string  `gorm:"not null"` // "unscanned", "scanned", "timeout"
+	Code          *int    // HTTP status code
+	ContentLength *int    // in bytes
 }
